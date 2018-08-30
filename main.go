2102ede94af985a04de92be03e066973539c34f2
王贺天 <hetian@hetian.me>
2018-08-30 23:04:30 +0800

    增加设置默认键名

--- main.go
+++ main.go
@@ -15,9 +15,10 @@ import (
 
 var (
 	ModelName   = flag.String("name", "users", "-name=模块名 如Users")
-	ApiUrl      = flag.String("api", "https://h5.cnganen.cn/test.json", "-api=你的API地址")
+	ApiUrl      = flag.String("api", "http://127.0.0.1:3000/api2.0/app.users.base", "-api=你的API地址")
 	Path        = flag.String("page", "./dart", "-path=文件保存位置")
 	packagePath = flag.String("package", "./", "-package=引用包前缀")
+	defDataName = flag.String("mapname", "data", "-mapname=默认的数据键名")
 )
 
 func main() {
@@ -61,6 +62,12 @@ func main() {
 		logrus.Error("接口返回可能非正确的JSON格式, ", err)
 		return
 	}
-	convert.NewConvert(*Path, *packagePath, *ModelName, jsonMap)
+	var defData interface{};
+	if (*defDataName != "") {
+		defData = jsonMap.(map[string]interface{})[*defDataName]
+	}else{
+		defData = jsonMap;
+	}
+	convert.NewConvert(*Path, *packagePath, *ModelName, defData)
 	fmt.Println("搞定收工")
 }
